14c19570663de1de1c67e08f7d0370ea770250ca
Shibani Dhume <shibanidhume@yahoo.com>
2013-04-12 17:39:33 -0700

    created repo
    put in test1 files inside this new repo

---
+++ Test1/src/com/example/test1/Set_Emotions.java
@@ -0,0 +1,17 @@
+package com.example.test1;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.app.Activity;
+
+public class Set_Emotions extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.set_emotions);
+		
+	}
+
+
+}
