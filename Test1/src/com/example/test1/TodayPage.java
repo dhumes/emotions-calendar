59d368ede35ffdeeba1c6180a5469bc1be29fce1
Joey Shepard <joey_shepard@sbcglobal.net>
2013-04-15 09:29:01 -0700

    Updated buttons/connections

--- Test1/src/com/example/test1/TodayPage.java
+++ Test1/src/com/example/test1/TodayPage.java
@@ -1,8 +1,13 @@
 package com.example.test1;
 
+import java.util.ArrayList;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class TodayPage extends Activity
@@ -13,6 +18,36 @@ public class TodayPage extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.todaypagelayout);
+		
+		Button MorningSet = (Button) findViewById(R.id.button1);
+		Button MorningChange = (Button) findViewById(R.id.button4);
+		
+		Button AfternoonSet = (Button) findViewById(R.id.button2);
+		Button AfternoonChange = (Button) findViewById(R.id.button5);
+		
+		Button EveningSet = (Button) findViewById(R.id.button3);
+		Button EveningChange = (Button) findViewById(R.id.button6);
+		
+		ArrayList<Button> buttonsToFeelingSelect = new ArrayList<Button>();
+		buttonsToFeelingSelect.add(MorningSet);
+		buttonsToFeelingSelect.add(MorningChange);
+		buttonsToFeelingSelect.add(AfternoonSet);
+		buttonsToFeelingSelect.add(AfternoonChange);
+		buttonsToFeelingSelect.add(EveningSet);
+		buttonsToFeelingSelect.add(EveningChange);
+		
+		OnClickListener toFeelingSelect = new OnClickListener(){
+			public void onClick(View v)
+			{
+				Intent nextIntent = new Intent(TodayPage.this, MainActivity.class);
+				TodayPage.this.startActivity(nextIntent);
+			}
+		};
+		
+		for (Button b : buttonsToFeelingSelect)
+		{ b.setOnClickListener(toFeelingSelect); }
+				
+				
 	}
 
 
@@ -22,6 +57,31 @@ public class TodayPage extends Activity
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.today_page, menu);
 		return true;
+	}
+	
+	protected void onStart()
+	{
+		super.onStart();
+	}
+	
+	
+	protected void onRestart()
+	{
+		super.onRestart();
+	}
+	
+	protected void onStop()
+	{
+		super.onStop();
+	}
+	protected void onResume()
+	{
+		super.onResume();
+	}
+	
+	protected void onDestroy()
+	{
+		super.onDestroy();
 	}
 
 }
