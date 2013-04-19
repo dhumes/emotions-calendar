917aeb2c451a61340a411b06f319d9a4b0bea861
Jacobus <jahardin@uci.edu>
2013-04-19 12:45:50 -0700

    homescreen .java file updated to allow menu seletion on buttons

--- Team20/src/com/example/test1/MyHomeScreen.java
+++ Team20/src/com/example/test1/MyHomeScreen.java
@@ -3,44 +3,52 @@ package com.example.test1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MyHomeScreen extends Activity {
+
+	public static String[] months = {"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec",
+	};
 	
+	public static int month;
+	public static int day;
+	public static int year;
+	public static String menu_type;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
 		setContentView(R.layout.homescreenlayout);
-		
-		Button toPage1 = (Button) findViewById(R.id.toPage1);
-		Button toPage = (Button) findViewById(R.id.toPage2);
-		toPage1.setText("Go to page 1");
-		toPage1.setOnClickListener(new OnClickListener(){
-			public void onClick(View v)
-			{
-				Intent nextIntent = new Intent(MyHomeScreen.this, MainActivity.class);
-				MyHomeScreen.this.startActivity(nextIntent);
-			}
-		});
-		
-		
-		
-	}
-	
+
+	};
+
 	protected void onStart()
 	{
 		super.onStart();
 	}
-	
-	
+
+
 	protected void onRestart()
 	{
 		super.onRestart();
 	}
-	
+
 	protected void onStop()
 	{
 		super.onStop();
@@ -49,10 +57,76 @@ public class MyHomeScreen extends Activity {
 	{
 		super.onResume();
 	}
-	
+
 	protected void onDestroy()
 	{
 		super.onDestroy();
+	}
+	
+	@Override
+    public void onCreateContextMenu(ContextMenu menu, View view, 
+    ContextMenuInfo menuInfo) 
+    {
+         super.onCreateContextMenu(menu, view, menuInfo);
+         if (view == (Button) findViewById(R.id.bday)){
+        	 menu.setHeaderTitle("Select Day");
+        	 menu_type = "day";
+        	 CreateDayMenu(menu);
+         }
+         if (view == (Button) findViewById(R.id.bmonth)){
+        	 menu.setHeaderTitle("Select Month");
+        	 menu_type = "month";
+        	 CreateMonthMenu(menu);
+         }
+         if (view == (Button) findViewById(R.id.start_year)){
+        	 menu.setHeaderTitle("Select Year");
+        	 menu_type = "year";
+        	 CreateYearMenu(menu);
+         }
+    }
+
+	private void CreateDayMenu(Menu menu)
+	{
+		menu.setQwertyMode(true);
+		int num_days;
+		switch(month){
+		case 1:
+			num_days = 28;
+			break;
+		case 3:
+			num_days = 30;
+			break;
+		case 5:
+			num_days = 30;
+			break;
+		case 8:
+			num_days = 30;
+			break;
+		case 10:
+			num_days = 30;
+			break;
+		default:
+			num_days = 31;
+			break;
+		}
+		for (int i = 1; i<=num_days; ++i){
+			menu.add(0,i,i,Integer.toString(i));
+		}
+	}
+	
+	private void CreateMonthMenu(Menu menu)
+    {
+    	 menu.setQwertyMode(true);
+    	 for (int i = 0; i<12; ++i){
+    		 menu.add(0,i,i,months[i]);
+    	 }
+     }
+	
+	private void CreateYearMenu(Menu menu){
+		menu.setQwertyMode(true);
+		for (int i = 0; i<5; ++i){
+			menu.add(0,i,i,Integer.toString(year-i));
+		}
 	}
 
 }
